00f1b47085dd6e56e8dfae52ef60449711dffed6
Pengfei Liu <liu.pengfei@hotmail.fr>
2023-05-31 10:09:09 +0200

    add file logging

--- tools/src/data_format_convertor.py
+++ tools/src/data_format_convertor.py
@@ -7,22 +7,40 @@ from pyspark.sql import SparkSession
 import argparse
 import pathlib
 import chardet
+import logging
 
 CSV_FORMAT = "csv"
 SAS_FORMAT = "sas7bdat"
+LOG_LEVEL = logging.INFO
+LOG_FILE_PATH = "/tmp/format_convertor/app.log"
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+
+# Set up logging configuration
+logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
+
+# Create file handler
+file_handler = logging.FileHandler(LOG_FILE_PATH)
+file_handler.setLevel(LOG_LEVEL)
+file_formatter = logging.Formatter(LOG_FORMAT)
+file_handler.setFormatter(file_formatter)
+
+# Create stream handler (console)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(LOG_LEVEL)
+stream_formatter = logging.Formatter(LOG_FORMAT)
+stream_handler.setFormatter(stream_formatter)
+
+# Get the root logger and add the handlers
+logger = logging.getLogger('')
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 
 def convertFilesToParquet(spark: SparkSession, rootPath: str, outDirPath: str, **kwargs):
     """
     This function takes an input folder, scans all the file in it, if its extension is csv or sas, it will be converted
-    to parquet
+    to parquet. It can take a list of implicite argument such as delimiter, encoding, partitionColumns.
 
-    :param delimiter:
-    :type delimiter:
-    :param partitionColumns:
-    :type partitionColumns:
-    :param encoding:
-    :type encoding:
     :param spark:
     :type spark:
     :param rootPath:
@@ -33,7 +51,7 @@ def convertFilesToParquet(spark: SparkSession, rootPath: str, outDirPath: str, *
     :rtype:
     """
     if not path.isdir(rootPath):
-        print("The given path is not a directory")
+        logging.warning("The given path is not a directory")
         sys.exit(1)
 
     for root, dirs, files in walk(rootPath):
@@ -74,20 +92,22 @@ def convertSasToParquet(spark: SparkSession, filePath: str, outPath: str,
     :return:
     :rtype:
     """
-    if not checkFileFormat(filePath, "sas"):
-        print("Please enter a valid sas file path")
+    logging.info(f"Start to convert sas file {filePath} to parquet file at {outPath}")
+    if not checkFileFormat(filePath, SAS_FORMAT):
+        logging.warning("Please enter a valid sas file path")
     try:
         df = spark.read \
             .format("com.github.saurfang.sas.spark") \
             .load(filePath, forceLowercaseNames=True, inferLong=True)
         df.show(5)
     except Exception as e:
-        print(f"Can't read the given path as a sas file with below exception {e}")
+        logging.error(f"Can't read the given path as a sas file with below exception {e}")
         raise ValueError(f"The given path is not in sas format")
     if partitionColumns:
         df.write.partitionBy(partitionColumns).mode("overwrite").parquet(outPath)
     else:
         df.write.mode("overwrite").parquet(outPath)
+    logging.info(f"Finish the sas file conversion")
 
 
 def convertCsvToParquet(spark: SparkSession, filePath: str, outPath: str, delimiter: str = ",", encoding: str = "utf-8",
@@ -109,9 +129,9 @@ def convertCsvToParquet(spark: SparkSession, filePath: str, outPath: str, delimi
     :return: None
     :rtype:
     """
-
-    if not checkFileFormat(filePath, "csv"):
-        print("Please enter a valid csv file path")
+    logging.info(f"Start to convert csv file {filePath} to parquet file at {outPath}")
+    if not checkFileFormat(filePath, CSV_FORMAT):
+        logging.error("Please enter a valid csv file path")
         raise ValueError(f"The given path is not in csv format")
 
     try:
@@ -123,18 +143,19 @@ def convertCsvToParquet(spark: SparkSession, filePath: str, outPath: str, delimi
             .csv(filePath)
         df.show(5)
     except Exception as e:
-        print(f"Can't read the given path as a csv file with below exception {e}")
+        logging.error(f"Can't read the given path as a csv file with below exception {e}")
         raise ValueError(f"The given path is not in csv format")
     if partitionColumns:
         df.write.partitionBy(partitionColumns).mode("overwrite").parquet(outPath)
     else:
         df.write.mode("overwrite").parquet(outPath)
+    logging.info(f"Finish the csv file conversion")
 
 
 def checkFileFormat(filePath: str, expectedFormat: str):
     inFile = pathlib.Path(filePath)
     if not inFile.is_file():
-        print(f"The given path {filePath} is not a file")
+        logging.warning(f"The given path {filePath} is not a file")
         return False
 
     if expectedFormat.lower() == CSV_FORMAT:
@@ -155,8 +176,7 @@ def checkCSVEncoding(filePath: str):
         result = chardet.detect(rawData)
         encoding = result['encoding']
         confidence = result['confidence']
-        print(type(confidence))
-        print(f"Inferred encoding value: {encoding}, with confidence: {confidence}")
+        logging.info(f"Inferred encoding value: {encoding}, with confidence: {confidence}")
         if confidence > 0.6:
             return encoding
         else:
@@ -169,10 +189,11 @@ def main():
     parser = argparse.ArgumentParser(description="This CLI can help you to convert sas or csv files to parquet format")
     parser.add_argument("inputPath", help="The root directory path which contains the csv or sas files")
     parser.add_argument("outputPath", help="The root directory path which contains the output parquet files")
-    parser.add_argument("--isFile", help="By default this option is set to False, If this option is enabled "
-                                         "by putting True, the application takes one single file path as input")
+    # parser.add_argument("--isFile", help="By default this option is set to False, If this option is enabled "
+    #                                      "by putting True, the application takes one single file path as input")
     parser.add_argument("--delimiter", help="If the input csv files does not use the default delimiter ',', user need "
-                                            "to provide the delimiter value")
+                                            "to provide the delimiter value. Important note, as python argparse "
+                                            "considers `;` as separator also, you need to add quotes to semicolon")
     parser.add_argument("--encoding", help="if the input csv file does not use the default encoding value utf-8, user "
                                            "need to provide the encoding value")
     parser.add_argument("--partitionColumns", help="If no partition columns is provided, use the default hash "
@@ -184,11 +205,16 @@ def main():
     args = parser.parse_args()
     inputPath = args.inputPath
     outputPath = args.outputPath
-    isFile = args.isFile == "True" or "False"
+    # isFile = args.isFile == "True" or "False"
     delimiter = args.delimiter or ","
     encoding = args.encoding or "utf-8"
-    partitionColumns = str(args.partitionColumns).split(",") or None
-
+    # note if the optional arg is empty, a None will be returned
+    if args.partitionColumns:
+        partColumns = str(args.partitionColumns).split(",")
+    else:
+        partColumns = None
+    logging.info(f"User input delimiter value: '{delimiter}', encoding value: {encoding}, "
+                 f"partition columns : {partColumns}")
     # get dependent jar path
     rootLibPath = pathlib.Path.cwd().parent.parent / "libs"
     parsoPath = rootLibPath / "parso-2.0.11.jar"
@@ -200,19 +226,27 @@ def main():
     # the shuffle partition number should be the same as the executor number
     # The default max spark partition size is 128MB, which may result in too many small partition.
     # This lead to small task that slows down the performance.
+    logging.info("Building spark session")
     spark = SparkSession.builder.master("local[*]") \
         .config("spark.driver.memory", "8g") \
         .config("spark.sql.shuffle.partitions", "8") \
         .config("spark.sql.files.maxPartitionBytes", "256mb") \
         .config("spark.jars", jarPath) \
         .appName("DataFormatConvertor").getOrCreate()
-
-    if isFile:
-        convertFileToParquet(spark, inputPath, outputPath, delimiter=delimiter, encoding=encoding,
-                             partitionColumns=partitionColumns)
+    logging.info("Start file conversion")
+    if path.isfile(inputPath):
+        if partColumns:
+            convertFileToParquet(spark, inputPath, outputPath, delimiter=delimiter, encoding=encoding,
+                                 partitionColumns=partColumns)
+        else:
+            convertFileToParquet(spark, inputPath, outputPath, delimiter=delimiter, encoding=encoding)
     else:
-        convertFilesToParquet(spark, inputPath, outputPath, delimiter=delimiter, encoding=encoding,
-                              partitionColumns=partitionColumns)
+        if partColumns:
+            convertFilesToParquet(spark, inputPath, outputPath, delimiter=delimiter, encoding=encoding,
+                                  partitionColumns=partColumns)
+        else:
+            convertFilesToParquet(spark, inputPath, outputPath, delimiter=delimiter, encoding=encoding)
+    logging.info("End file conversion")
 
 
 if __name__ == "__main__":
